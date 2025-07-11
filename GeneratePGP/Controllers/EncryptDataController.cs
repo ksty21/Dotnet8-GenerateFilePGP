3058b0386c588b003701d94d9cf48d198c7b709f
ksty21 <kustoyo@teladanprima.com>
2025-07-11 15:05:31 +0700

    Add project files.

---
+++ GeneratePGP/Controllers/EncryptDataController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace GeneratePGP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EncryptDataController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public EncryptDataController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet("download-pgp")]
+        public IActionResult GetEncryptedCsv()
+        {
+            // Contoh data CSV
+            var csv = "Nama,NIK,Paket,Harga\nAgus,1607150104941002,15 Mbps,170000";
+            var csvBytes = Encoding.UTF8.GetBytes(csv);
+
+            // Lokasi public key
+            //var publicKeyPath = Path.Combine(_env.WebRootPath, "pgp-keys", "public_key_dummy.asc");
+            var publicKeyPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "pgp-keys", "public_key_mcm.asc");
+
+
+            // Enkripsi
+            var pgpBytes = Helper.Encrypt(csvBytes, publicKeyPath);
+
+            return File(pgpBytes, "application/octet-stream", "data.csv.pgp");
+        }
+    }
+}
---
+++ GeneratePGP/Helper/Helper.cs
@@ -0,0 +1,39 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using Org.BouncyCastle.Security;
+using System.IO;
+
+public static class Helper
+{
+    public static byte[] Encrypt(byte[] data, string publicKeyPath)
+    {
+        using var outputStream = new MemoryStream();
+        using var publicKeyStream = File.OpenRead(publicKeyPath);
+        var pubKeyRingBundle = new PgpPublicKeyRingBundle(PgpUtilities.GetDecoderStream(publicKeyStream));
+        var pubKey = GetEncryptionKey(pubKeyRingBundle);
+
+        var encryptedDataGenerator = new PgpEncryptedDataGenerator(
+            Org.BouncyCastle.Bcpg.SymmetricKeyAlgorithmTag.Cast5, true, new SecureRandom());
+
+        encryptedDataGenerator.AddMethod(pubKey);
+
+        using (var encOut = encryptedDataGenerator.Open(outputStream, data.Length))
+        {
+            encOut.Write(data, 0, data.Length);
+        }
+
+        return outputStream.ToArray();
+    }
+
+    private static PgpPublicKey GetEncryptionKey(PgpPublicKeyRingBundle keyRingBundle)
+    {
+        foreach (PgpPublicKeyRing keyRing in keyRingBundle.GetKeyRings())
+        {
+            foreach (PgpPublicKey key in keyRing.GetPublicKeys())
+            {
+                if (key.IsEncryptionKey)
+                    return key;
+            }
+        }
+        throw new ArgumentException("No encryption key found in public key file.");
+    }
+}
